6b289c008263dcad9ec4f60265e5cc34e455bf78
gercke <45533947+gercke@users.noreply.github.com>
2021-02-04 23:30:23 +0800

    🧐链表中倒数第k个结点

---
+++ jz-22.js
@@ -0,0 +1,13 @@
+var getKthFromEnd = function(head, k) {
+    let p = head, q = head;
+
+    let i = 0;
+    while (p) {
+        if (i >= k) {
+            q = q.next;
+        }
+        p = p.next;
+        i++;
+    }
+    return i < k ? null : q;
+};
